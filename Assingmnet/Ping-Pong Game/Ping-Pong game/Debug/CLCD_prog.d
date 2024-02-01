99e722031507bd694719a62b0a49f963d966cae1
Ahmed Faheem <43971128+ahmedfaheem@users.noreply.github.com>
2024-02-01 18:08:50 +0200

    Ping-Pong game using LCD and external interrupt.
    Display a ball moving on LCD from one end to the other.
    Two players are playing (one on each side) with two buttons ( the button represents the Ping pong racket )
    Use external interrupt to indicate whether the button is pressed or not.
    If the player press the button, the ball should bounce to the other side
    If not pressed the opposite player wins the game

---
+++ Assingmnet/Ping-Pong Game/Ping-Pong game/Debug/CLCD_prog.d
@@ -0,0 +1,17 @@
+CLCD_prog.o CLCD_prog.o: ../CLCD_prog.c ../STD_TYPES.h ../BIT_MATH.h \
+  ../ErrType.h ../DIO_interface.h ../CLCD_interface.h ../CLCD_cfg.h \
+  ../CLCD_prv.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../ErrType.h:
+
+../DIO_interface.h:
+
+../CLCD_interface.h:
+
+../CLCD_cfg.h:
+
+../CLCD_prv.h:
